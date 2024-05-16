7b6ed5873256ebd6b0f049083a4cae65684137dd
azizahmad <azizahmade318@gmail.com>
2024-05-16 04:58:05 +0430

    app updated

--- App.py
+++ App.py
@@ -1,16 +1,21 @@
 from scapy.all import *
 from VPNProtocolDetection import VPNProtocolDetector
 from VPNServerIP import VPNServerIpDetector
-
+from CIP_P2P import CIP_P2P
 
 if __name__ == "__main__":
     packets = sniff(iface="wlp2s0", count=10)
 
-    pd = VPNProtocolDetector()
-    pd.packet_callback(packets)
+    # output_file = "captured_packets.pcap"
+    # wrpcap(output_file, packets)
 
-    ipd = VPNServerIpDetector()
-    ipd.analyze_packet(packets) 
-    
+    # pd = VPNProtocolDetector()
+    # pd.packet_callback(packets)
+
+    # ipd = VPNServerIpDetector()
+    # ipd.analyze_packet(packets)
+
+    ri = CIP_P2P()
+    ri.analyze_packet(packets)
           
 
